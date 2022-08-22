9788bea56a78adf718e449e09fa7f3392ac3ac82
shreedhar41 <shreedharac41@gmail.com>
2022-08-22 10:39:18 +0530

    afshj dgkhu

---
+++ src/java11/abc.java
@@ -0,0 +1,44 @@
+
+package java11;
+
+public class abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("xyz");
+System.out.println(123);
+System.out.println("x");
+System.out.println(true);
+System.out.println(125.50);
+System.out.println(125f);
+int a=10;
+int b=20;
+System.out.println(a+b);
+char ab ='x';
+//int c = null;
+String xl = null;
+int[] x = {1,2,3,4,5};
+Integer abcd= null;
+System.out.println("ADSSS");
+byte x1 = 127;
+byte x2 = 10;
+System.out.println(x1+x2);
+byte x3 = 127;  
+x3++;
+System.out.println(x3);
+byte x4 = 127;  
+++x4;
+byte x5 = 127;  
+byte x6 = 127;
+x6--;
+System.out.println(x6);
+
+
+
+
+
+
+
+	}
+
+}
