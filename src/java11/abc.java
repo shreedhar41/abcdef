366e5a5e9127bda9241195ef5e6702f968db670e
shreedhar41 <shreedharac41@gmail.com>
2022-08-29 12:03:34 +0530

    changed

--- src/java11/abc.java
+++ src/java11/abc.java
@@ -14,31 +14,7 @@ System.out.println(125f);
 int a=10;
 int b=20;
 System.out.println(a+b);
-char ab ='x';
-//int c = null;
-String xl = null;
-int[] x = {1,2,3,4,5};
-Integer abcd= null;
-System.out.println("ADSSS");
-byte x1 = 127;
-byte x2 = 10;
-System.out.println(x1+x2);
-byte x3 = 127;  
-x3++;
-System.out.println(x3);
-byte x4 = 127;  
-++x4;
-byte x5 = 127;  
-byte x6 = 127;
-x6--;
-System.out.println(x6);
-
-
-
-
-
-
-
-	}
-
+System.out.println(hiii);}
 }
+
+
